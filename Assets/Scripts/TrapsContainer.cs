425ec9f02ee9f25e3948ccd6d954fe08b00a1ce9
amber k <38854883+daddyaltman@users.noreply.github.com>
2023-03-14 09:31:27 +0200

    maze base

---
+++ Assets/Scripts/TrapsContainer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapsContainer : ActiveTrap
+{
+    public override void ApplyDamage(IDamagable damagable)
+    {
+     
+    }
+}
